89d71b2b9e6d09225c0a3177970d03cb9a719d24
Erick Silva <engs.3003@gmail.com>
2024-06-25 18:08:43 -0300

    checking if the user got the answer correctly, fix 'format_data' func description

--- main.py
+++ main.py
@@ -2,11 +2,21 @@ import art, random, os
 from game_data import data
 
 def format_data(account):
-    """Format the data into printable format"""
+    """Format the account data into printable format"""
     account_name = account["name"]
     account_descr = account["description"]
     account_country = account["country"]
     return f"{account_name}, a {account_descr}, from {account_country}"
+
+def check_guess(guess, a_followers, b_followers):
+    """Checks the user answer with an if statement to verify if it was right"""
+    if a_followers > b_followers:
+        return guess == "a"
+    elif b_followers > a_followers:
+        return guess == "b"
+
+
+
 
 # Printin game logo
 print(art.logo)
@@ -25,3 +35,7 @@ print(art.vs)
 print(f"Against B: {format_data(account_b)}")
 
 guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+a_follower_count = account_a["follower_count"]
+b_follower_count = account_b["follower_count"]
+
+is_right = check_guess(guess, a_follower_count, b_follower_count)
