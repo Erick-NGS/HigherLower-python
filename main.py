e8320ab9fc8173d56f84b6ead08e53bce9fe25e2
Erick Silva <engs.3003@gmail.com>
2024-06-25 17:46:50 -0300

    formatting the data to be presented in the program

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-import game_data, art
+import art, random, os
+from game_data import data
 
+def format_data(account):
+    """Format the data into printable format"""
+    account_name = account["name"]
+    account_descr = account["description"]
+    account_country = account["country"]
+    return f"{account_name}, a {account_descr}, from {account_country}"
+
+# Printin game logo
 print(art.logo)
+
+# os.system('cls||clear')
+
+# Selecting 2 random assets from 'game_data.data'
+account_a = random.choice(data)
+account_b = random.choice(data)
+# Checking to make sure that both accounts are different from each other
+if account_a == account_b:
+    account_b = random.choice(data)
+
+print(f"Compare A: {format_data(account_a)}")
+print(art.vs)
+print(f"Compare B: {format_data(account_b)}")
